17e335af3cc8b02819ca587935c2876a743f5f60
Hariom-vishwa <hawkgamerhv@gmail.com>
2026-05-06 20:18:39 +0530

    Update Projects and Skills components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css"
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import "./Style/Skills.css";
-import "../index.css";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
