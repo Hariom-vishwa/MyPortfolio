6c8a4bbd6b91e7624928c7632b6aa4cf61060f42
Hariom-vishwa <hawkgamerhv@gmail.com>
2026-05-09 21:45:03 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useEffect, useRef, useState } from "react";
 import Loader from "./components/Loader";
-import { MenuSquare } from "lucide-react";
 
 gsap.registerPlugin(useGSAP);
 
